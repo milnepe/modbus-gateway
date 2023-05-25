9cceddda637a6d5798aff136f92b0952d98d0bed
Peter Milne <milnepe@gmail.com>
2023-05-25 17:36:35 +0100

    Add smoke test

---
+++ smoke_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import minimalmodbus as mm
+from time import sleep
+
+ON = 1
+OFF = 0
+
+plc = mm.Instrument('/dev/ttymxc3', 1)
+plc.serial.baudrate = 9600
+
+while True:
+    # Toggle outputs Q1 - Q4 in sequence 
+    for coil in range(0, 4):
+        plc.write_bit(coil, ON)
+        print(f'Q{coil + 1}, {plc.read_bit(coil, functioncode=1)}')
+        sleep(1)
+        plc.write_bit(coil, OFF)
+        print(f'Q{coil + 1}, {plc.read_bit(coil, functioncode=1)}')
+        sleep(1)
